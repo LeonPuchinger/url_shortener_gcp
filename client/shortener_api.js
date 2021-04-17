3b1257482771d83f301af6fa1e65e83274360802
LeonPuchinger <lepu1590@gmail.com>
2021-04-17 21:14:42 +0200

    client display shortened url or error

--- client/shortener_api.js
+++ client/shortener_api.js
@@ -1,12 +1,38 @@
 const API_URL = "http://localhost:8080"; //just for testing
 
+function hostname() {
+    return window.location.hostname
+}
+
+function display_shortened(base, key, url) {
+    var element = `<li class="list-group-item list-group-item-info">
+        <a href="http://${base}/${key}" class="alert-link" style="display: block">${base}/${key}</a>
+        <a href="${url}" class="link-secondary">${url}</a>
+    </li>
+    `
+    $("#url-list").prepend(element)
+}
+
+function display_error(url) {
+    var element = `<li class="list-group-item list-group-item-danger">
+        <p>Error: Unable to shorten url</p>
+        <a href="${url}" class="link-secondary">${url}</a>
+    </li>
+    `
+    $("#url-list").prepend(element)
+}
+
 async function shorten() {
-    const url = $("#urlInput").val();
+    const url = $("#url-input").val();
     const encoded = encodeURIComponent(url);
-    const result = await $.ajax({
-        type: "POST",
-        url: `${API_URL}/add?url=${encoded}`,
-        dataType: "text",
-    });
-    console.log(result);
+    try {
+        const result = await $.ajax({
+            type: "POST",
+            url: `${API_URL}/add?url=${encoded}`,
+            dataType: "json",
+        });
+        display_shortened(hostname(), result["key"], result["url"])
+    } catch (_) {
+        display_error(url)
+    }
 }

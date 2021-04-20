1a392505f3a0e757ef6d6fca16c6c7ffc2ec95c0
LeonPuchinger <lepu1590@gmail.com>
2021-04-20 15:12:59 +0200

    client submit button loading animation & comments

--- client/shortener_api.js
+++ client/shortener_api.js
@@ -1,4 +1,15 @@
-const API_URL = "https://api-server-3ucl2t344q-ue.a.run.app";
+const API_URL = "https://api-server-3ucl2t344q-ue.a.run.app"
+
+//setup submit button enabled/disabled state depending on if there is input or not
+function init() {
+    const url = $("#url-input").on("input", function () {
+        if ($("#url-input").val() == "") {
+            $("#submit-button").prop("disabled", true)
+        } else {
+            $("#submit-button").prop("disabled", false)
+        }
+    })
+}
 
 function hostname() {
     // only needed if client is hosted under same domain as api/server backend
@@ -6,6 +17,7 @@ function hostname() {
     return API_URL
 }
 
+// display returned shortened and original url
 function display_shortened(base, key, url) {
     var element = `<li class="list-group-item list-group-item-info">
         <a href="${base}/${key}" class="alert-link" style="display: block">${base}/${key}</a>
@@ -15,6 +27,7 @@ function display_shortened(base, key, url) {
     $("#url-list").prepend(element)
 }
 
+// display error message and original url (if any)
 function display_error(url, error) {
     var element = `<li class="list-group-item list-group-item-danger">
         <p>${error}</p>
@@ -24,15 +37,31 @@ function display_error(url, error) {
     $("#url-list").prepend(element)
 }
 
+// show loading animation
+function enable_loading() {
+    $("#submit-spinner").css("display", "inline-block")
+    $("#submit-text").text("Loading...")
+    $("#submit-button").prop("disabled", true)
+}
+
+// hide loading animation
+function disable_loading() {
+    $("#submit-spinner").css("display", "none")
+    $("#submit-text").text("Shorten!")
+    $("#submit-button").prop("disabled", false)
+}
+
+// call api with input url, start/stop loading animation, display response/error
 async function shorten() {
-    const url = $("#url-input").val();
-    const encoded = encodeURIComponent(url);
+    enable_loading()
+    const url = $("#url-input").val()
+    const encoded = encodeURIComponent(url)
     try {
         const result = await $.ajax({
             type: "POST",
             url: `${API_URL}/add?url=${encoded}`,
             dataType: "json",
-        });
+        })
         display_shortened(hostname(), result["key"], result["url"])
     } catch (error) {
         var msg
@@ -43,5 +72,7 @@ async function shorten() {
             msg = `Error: ${error.responseJSON["error"]}`
         }
         display_error(url, msg)
+    } finally {
+        disable_loading()
     }
 }
